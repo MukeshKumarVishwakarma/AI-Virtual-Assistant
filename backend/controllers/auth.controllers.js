eeba1a6561939e9a028dc0d4f96f4b110f716a9c
mukesh <mukeshvishwakarma251003@gmail.com>
2025-09-11 19:16:09 +0530

    some change

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -37,8 +37,8 @@ export const signUp = async (req, res) => {
         res.cookie("token",token,{
             httpOnly: true,
             maxAge: 7*24*60*60*1000,
-            sameSite: "strict",
-            secure: false
+            sameSite: "None",
+            secure: true
         })
         
         return res.status(201).json({
@@ -83,8 +83,8 @@ export const login = async (req, res) => {
         res.cookie("token",token,{
             httpOnly: true,
             maxAge: 7*24*60*60*1000,
-            sameSite: "strict",
-            secure: false
+            sameSite: "None",
+            secure: true
         })
         
         return res.status(200).json({
