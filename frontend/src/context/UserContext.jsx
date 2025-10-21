09e0936592df9803f468db20ea95d05867e164bb
MukeshKumarVishwakarma <145449533+MukeshKumarVishwakarma@users.noreply.github.com>
2025-10-21 18:20:01 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const userDataContext=createContext()
 
 function UserContext({children}) {
-    const serverUrl="http://localhost:2000"
+    const serverUrl="https://ai-virtual-assistant-backend-sgwj.onrender.com"
     const [userData, setUserData] = useState(null)
     const [frontendImage, setFrontendImage] = useState(null);
     const [backendImage, setBackendImage] = useState(null);
